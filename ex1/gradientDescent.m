cf463d4b42d1471003e4ce854c015cbb1da6e36b
Steve Bastin <sbastin@nerdery.com>
2015-03-06 14:18:15 -0600

    finished linear regression of single variable

--- ex1/gradientDescent.m
+++ ex1/gradientDescent.m
@@ -7,21 +7,29 @@ function [theta, J_history] = gradientDescent(X, y, theta, alpha, num_iters)
 m = length(y); % number of training examples
 J_history = zeros(num_iters, 1);
 
+
 for iter = 1:num_iters
 
-    % ====================== YOUR CODE HERE ======================
-    % Instructions: Perform a single gradient step on the parameter vector
-    %               theta. 
-    %
-    % Hint: While debugging, it can be useful to print out the values
-    %       of the cost function (computeCost) and gradient here.
-    %
-
-
-
-
-
-
+      % ====================== YOUR CODE HERE ======================
+      % Instructions: Perform a single gradient step on the parameter vector
+      %               theta. 
+      %
+      % Hint: While debugging, it can be useful to print out the values
+      %       of the cost function (computeCost) and gradient here.
+  
+    sum_theta0 = 0;
+    sum_theta1 = 0;
+    
+    for i = 1:m
+      xi = (X(i:i, :))'; 
+      yi = y(i);
+      xi_1 = xi(2,1);
+      sum_theta0 = sum_theta0 + (theta'*xi - yi);
+      sum_theta1 = sum_theta1 + (theta'*xi - yi) * xi_1;
+    end
+    
+    theta(1) = theta(1) - (alpha / m)*(sum_theta0);
+    theta(2) = theta(2) - (alpha / m)*(sum_theta1);
 
     % ============================================================
 
